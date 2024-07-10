43b21f5d597222134a3749f8e1467b3da68e7bf4
SidZRed <siddharthrr@iisc.ac.in>
2024-07-10 10:34:36 +0530

    Add changes for encryption

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]) {
             return 1;
         }
 
-        string inputFile = argv[2];
+        string inputFile = argv[3];
         string outputFile = inputFile + ".huff";
         string mapFile = inputFile + ".map";
 
@@ -43,7 +43,23 @@ int main(int argc, char* argv[]) {
     }
 
     if (command == "-encrypt"){
+        if (argc != 4) {
+            cout << "Invalid number of arguments for encrypt command.\n";
+            printUsage();
+            return 1;
+        }
 
+        string inputFile = argv[3];
+        string outputFile = inputFile + ".enc";
+        string keyFile = inputFile + ".key";
+
+        // Call encryption function
+        bool success = encryptFile(inputFile, outputFile, keyFile);
+        if (success) {
+            cout << "File encrypted successfully.\n";
+        } else {
+            cout << "Encryption failed.\n";
+        }
     }
     // Invalid command
     else {
