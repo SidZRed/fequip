a9797c39db499f90ce53345e61e86e0696e9f33e
SidZRed <siddharthrr@iisc.ac.in>
2024-07-12 18:55:28 +0530

    Add header file for decrypt

---
+++ src/decrypt/decrypt.h
@@ -0,0 +1,12 @@
+#ifndef DECRYPT_H
+#define DECRYPT_H
+
+#include <bits/stdc++.h>
+#include <sstream>
+
+using namespace std;
+
+// Function declarations
+int decryptFile(string inputFile, string outputFile, string keyFile, string type);
+
+#endif // DECRYPT_H
